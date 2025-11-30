e7de342acca87d936bb7f0897de3eeac9257cc1d
maudiaz221 <diazmauricio221@gmail.com>
2025-11-30 03:58:04 +0000

    removed unnecessary files

--- binance-producer/run_binance_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-export LOCAL_MODE=true
-export CRYPTO_SYMBOLS=BTCUSDT,ETHUSDT,BNBUSDT
-export BINANCE_STREAMS=trade,kline_1m
-./venv/bin/python binance_websocket_client.py
-
